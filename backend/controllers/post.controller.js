f6ebedc1a0fb10ab61fe770db36c4e884e961bc4
Tristanic07 <alainlado@gmail.com>
2025-10-05 20:40:50 +0800

    Post addition

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -51,6 +51,60 @@ export const getLikedPosts = async (req, res) => {
     }
 };
 
+export const getFollowingPosts = async (req, res) => {
+
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const following = user.following;
+
+        const feedPosts = await Post.find({ user: {$in: following} })
+        .sort({ createdAt: -1 })
+        .populate({
+            path: "user",
+            select: "-password"
+        })
+        .populate({
+            path: "comments.user",
+            select: "-password"
+        });
+
+        return res.status(200).json(feedPosts);
+    } catch (error) {
+        console.log("Error in getFollwingPosts: ", error.message);
+        res.status(500).json({error: error.message});  
+    }
+}
+
+export const getUserPosts = async (req, res) => {
+    try {
+        const { username } = req.params;
+
+        const user = await User.findOne({ username });
+        if(!user){
+            return res.status(404).json({error: "User not found"});
+        }
+
+        const posts = await Post.find({ user: user._id })
+        .sort({ createdAt: -1 })
+        .populate({
+            path: "comments.user",
+            select: "-password"
+        });
+
+        console.log(posts);
+
+        return res.status(200).json(posts);
+    } catch (error) {
+        console.log("Error in getUserPosts: ", error.message);
+        res.status(500).json({error: error.message});  
+    }
+};
+
 export const createPost = async (req, res) => {
     const { text } = req.body;
     let { img } = req.body;
