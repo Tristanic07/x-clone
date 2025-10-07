2e1bca4f8263fb9aa16f95d6df46824982051d30
Tristanic07 <alainlado@gmail.com>
2025-10-07 15:02:36 +0800

    Add Some Primary Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/SideBar";
+import RightPanel from "./components/common/RightPanel";
+
 function App() {
   return (
-    <>
-      <p className="text-5xl text-red-500 bg-blue-500">Hello World!</p>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <input
-        type="checkbox"
-        value="light"
-        className="toggle theme-controller"
-      />
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+
+      <RightPanel />
+    </div>
   );
 }
 
